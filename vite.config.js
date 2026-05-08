cb4b0ac0b201e757757247e035e71134b6a1a215
DanPorter1 <158497242+DanPorter1@users.noreply.github.com>
2026-05-08 10:20:11 +0100

    CF FIX

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import reactSWC from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+const isCloudflare = process.env.CF_PAGES === '1'
+
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: isCloudflare
+    ? [reactSWC()] // Cloudflare-safe build
+    : [
+        react(), // your local setup
+        babel({ presets: [react.reactCompilerPreset()] })
+      ]
 })
